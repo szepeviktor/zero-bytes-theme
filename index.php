158d5932c5c215917c3e3c87753dd213fdda9f72
Viktor Szépe <viktor@szepe.net>
2023-05-06 03:33:29 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+    <head>
+        <meta charset="<?php bloginfo( 'charset' ); ?>" />
+        <title><?php wp_title(); ?></title>
+        <!-- wp_head --><?php wp_head(); ?><!-- /wp_head -->
+    </head>
+    <body <?php body_class(); ?>>
+        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+        </article>
+        <!-- wp_footer --><?php wp_footer(); ?><!-- /wp_footer -->
+    </body>
+</html>
+
+*/
