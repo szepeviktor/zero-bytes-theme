c64bb895f3fd62ef5a3de8ae20cd0d7d0e6701d0
Viktor Szépe <viktor@szepe.net>
2024-12-22 11:46:50 +0000

    Improve HTML code

--- index.php
+++ index.php
@@ -6,16 +6,18 @@ if (defined('WP_DEBUG') && WP_DEBUG) :
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-    <meta charset="<?php bloginfo('charset'); ?>" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?php wp_title(); ?></title>
     <!-- wp_head --><?php wp_head(); ?><!-- /wp_head -->
 </head>
 <body <?php body_class(); ?>>
     <!-- wp_body_open --><?php wp_body_open(); ?><!-- /wp_body_open -->
     <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-<!-- theme_mods --><?php var_export(get_theme_mods()); ?><!-- /theme_mods -->
-<!-- theme_features --><?php global $_wp_theme_features; $_wp_theme_features === [] || var_export($_wp_theme_features); ?><!-- /theme_features -->
+        <!-- theme_mods --><pre><?php var_export(get_theme_mods()); ?></pre><!-- /theme_mods -->
+        <aside>
+            <!-- theme_features --><pre><?php global $_wp_theme_features; $_wp_theme_features === [] || var_export($_wp_theme_features); ?></pre><!-- /theme_features -->
+        </aside>
     </article>
     <!-- wp_footer --><?php wp_footer(); ?><!-- /wp_footer -->
 </body>
